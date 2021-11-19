8cdfde3476eaa1c748ecd7e9c003232ca5046aac
pigeatgarlic <64737125+pigeatgarlic@users.noreply.github.com>
2021-11-19 07:07:46 +0700

    add build

--- build.bat
+++ build.bat
@@ -2,3 +2,10 @@ call "C:\Program Files\Microsoft Visual Studio\2022\Preview\VC\Auxiliary\Build\v
 cd build
 cmake ..
 msbuild ALL_BUILD.vcxproj
+
+
+
+cd ..
+cd remote-ui/build
+C:\Qt\6.2.1\mingw81_64\bin\qmake.exe ../remote-ui.pro
+C:\Qt\Tools\mingw810_64\bin\mingw32-make.exe
