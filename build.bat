439027d0a23e12f0426a3af333e97ba30a82ea32
epitchi <thienvanlea1@gmail.com>
2021-11-23 23:35:10 +0700

    🦄 Handle build app

--- build.bat
+++ build.bat
@@ -1,11 +1,12 @@
-call "C:\Program Files\Microsoft Visual Studio\2022\Preview\VC\Auxiliary\Build\vcvarsall.bat" x86_amd64
-cd build
-cmake ..
-msbuild ALL_BUILD.vcxproj
+@REM call "C:\Program Files\Microsoft Visual Studio\2022\Preview\VC\Auxiliary\Build\vcvarsall.bat" x86_amd64
+@REM cd build
+@REM cmake ..
+@REM msbuild ALL_BUILD.vcxproj
 
 
 
-cd ..
-cd remote-ui/build
-C:\Qt\6.2.1\mingw81_64\bin\qmake.exe ../remote-ui.pro
-C:\Qt\Tools\mingw810_64\bin\mingw32-make.exe
+@REM call "D:\Qt\5.12.2\mingw73_64\bin\qtenv2.bat"
+@REM cd D:/Thinkmay-client/remote-ui/build
+@REM qmake ..
+@REM D:\Qt\Tools\mingw730_64\bin\mingw32-make.exe
+ 
