12d494549a265268145272ce263b40ecc5c55a4c
pigeatgarlic <64737125+pigeatgarlic@users.noreply.github.com>
2021-11-30 07:19:44 +0700

    fix cmakelist

--- build.bat
+++ build.bat
@@ -1,5 +1,5 @@
-@REM call "C:\Program Files\Microsoft Visual Studio\2022\Preview\VC\Auxiliary\Build\vcvarsall.bat" x86_amd64
-call "C:\Program Files (x86)\Microsoft Visual Studio\2019\Community\Common7\Tools\VsDevCmd.bat" x86_amd64
+call "C:\Program Files\Microsoft Visual Studio\2022\Preview\VC\Auxiliary\Build\vcvarsall.bat" x86_amd64
+@REM call "C:\Program Files (x86)\Microsoft Visual Studio\2019\Community\Common7\Tools\VsDevCmd.bat" x86_amd64
 cd build
 cmake .. 
 msbuild ALL_BUILD.vcxproj
